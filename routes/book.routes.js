6c712ea2a973143d98f7da16b722d36c1ee11791
alejandro blanco <alejandro@alejandros-MacBook-Air-2.local>
2022-04-25 14:50:14 +0200

    added book details by findbyid

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -9,7 +9,18 @@ router.get("/books", (req,res,next) => {
     .catch(err => console.log("ther is an error",err))
 });
 
+router.get("/books/:bookId", (req,res,next) => {
+    const id = req.params.bookId;
 
+    Book.findById(id)
+        .then((bookDetails) => {
+            res.render("books/book-details",{book:bookDetails})  //put the object to send more information
+        })
+        .catch( err => {
+            console.log("!error getting book details from DB", err)
+            next(err);
+        });
+})
 
 
 module.exports = router;
