95422dab1ffe888d21ff4603df0e5370db23bece
alejandro blanco <alejandro@alejandros-MacBook-Air-2.local>
2022-04-25 15:55:50 +0200

    created form to create books and add to DB

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -9,6 +9,34 @@ router.get("/books", (req,res,next) => {
     .catch(err => console.log("ther is an error",err))
 });
 
+//We place this one before the one below because in JS READING ORDER MATTERS
+
+
+router.get("/books/create", (req,res,next)  => {
+    res.render("books/books-create")
+})
+
+router.post("/books/create", (req,res,next) => {
+    
+    //create a object i¡in var instead of putting req.boy to be more specific
+    const newBook = {
+        title: req.body.title,
+        description: req.body.description,
+        author: req.body.author,
+        rating: req.body.rating,
+    }
+            Book.create(newBook)
+            .then((bookFromDB) => {
+                    res.redirect("/books")
+            })
+            .catch( err => {
+                console.log("error creating book from DB", err)
+                next(err);
+            });
+})
+
+
+
 router.get("/books/:bookId", (req,res,next) => {
     const id = req.params.bookId;
 
@@ -21,6 +49,11 @@ router.get("/books/:bookId", (req,res,next) => {
             next(err);
         });
 })
+
+
+
+
+
 
 
 module.exports = router;
