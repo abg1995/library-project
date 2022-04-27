86061f7d196b27b66a0c6e0e5d952875f167c13d
alejandro blanco <alejandro@alejandros-MacBook-Air-2.local>
2022-04-27 16:55:37 +0200

    user functionality and profile page

--- routes/authen.routes.js
+++ routes/authen.routes.js
@@ -12,6 +12,11 @@ router.post("/register", (req,res,next) => {
 
     const {email, password} = req.body;  //object destructuring ES6 js
 
+    if ( !email || !password){
+        res.render("auth/register", {errorMessage: "please provide email and password"});
+        return;
+    }
+
     bcryptjs.genSalt(saltRounds)
         .then(salt => {
             return bcryptjs.hash(password, salt);
@@ -36,6 +41,39 @@ router.post("/register", (req,res,next) => {
 
 })
 
+//LOGIN: display form
+router.get("/login", (req, res, next) => {
+    res.render("auth/login");
+})
 
+//LOGIN: display form
+
+router.post("/login", (req,res,next) => {
+
+    const {email, password} = req.body;
+
+    if( !email||!password){
+    res.render("auth/login", {errorMessage: 'Please provide email and password'});
+    return;
+    }
+
+    User.findOne({email: email})
+        .then( (userFromDB) => {
+            if( !userFromDB ){
+                res.render('auth/login', {errorMessage: 'Email is not registerede. Try with other email'});
+                return;
+            } else if (bcryptjs.compareSync(password, userFromDB.passwordHash)) {
+                res.send('login succesful')
+            }else{
+                res.render('auth/login', { errorMessage: 'Incorrect credentials'});
+            }
+        })
+        .catch(err => {
+            console.log("erro getting details from DB",error);
+            next(error);
+        });
+
+
+})
 
 module.exports = router;
